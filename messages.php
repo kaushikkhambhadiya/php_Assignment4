700ac44ad932fdde659c66778ad0501b9091d405
Kaushik <kaushikkhambhadiya.kk@gmail.com>
2021-03-22 12:30:07 -0400

    echo result

--- messages.php
+++ messages.php
@@ -6,5 +6,35 @@ require('mysqli_oop_connect.php');
 $q = "SELECT * from messages";
 $r = $mysqli->query($q);
 
+// echo statement which displays the username and the message
+$rowCount = $r->num_rows;
+if ($rowCount > 0) {
+    echo '<table class="table table-striped text-center">
+	<thead>
+	<tr>
+	<th>User Name</th>
+	<th>Messages</th>
+	</tr>
+	</thead>
+	<tbody>
+';
+
+    
+    while ($row = $r->fetch_object()) {
+        echo '<tr>
+              <td>' . $row->username . '</td>
+              <td>' . $row->message . '</td>
+              </tr>';
+    }
+
+    echo '</tbody>
+          </table>';
+
+} else {
+
+    echo '<p class="text-center text-danger">There are currently no users .</p>';
+
+}
+
 $mysqli->close();
 ?>
